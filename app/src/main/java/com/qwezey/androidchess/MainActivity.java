56b335997da9fe9562f0b4081d34a86d857d7921
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-12-07 16:49:25 -0500

    made it fullscreen

--- app/src/main/java/com/qwezey/androidchess/MainActivity.java
+++ app/src/main/java/com/qwezey/androidchess/MainActivity.java
@@ -2,12 +2,18 @@ package com.qwezey.androidchess;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
         setContentView(R.layout.activity_main);
     }
 }
