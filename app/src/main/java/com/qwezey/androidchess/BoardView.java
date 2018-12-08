04407926c566c295cc2b07b4c770956221008ca6
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-12-08 01:44:42 -0500

    squares become yellow on touch

--- app/src/main/java/com/qwezey/androidchess/BoardView.java
+++ app/src/main/java/com/qwezey/androidchess/BoardView.java
@@ -8,27 +8,29 @@ import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 
+import board.Board;
 import board.Coordinate;
 
 public class BoardView extends View {
 
-    Paint whitePaint;
-    Paint blackPaint;
+    private Board board;
+    private Square[][] grid;
+    private Canvas canvas;
 
-    public BoardView(Context context) {
-
+    public BoardView(Context context, Board board) {
         super(context);
-        whitePaint = new Paint();
-        whitePaint.setColor(Color.WHITE);
-        blackPaint = new Paint();
-        blackPaint.setColor(Color.GRAY);
+        this.board = board;
+        this.grid = new Square[8][8];
+        initializeGrid();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            System.out.println("(" + event.getX() + ", " + event.getY() + ")");
+            Coordinate c = getCoordinate(event.getX(), event.getY(), canvas.getWidth(), canvas.getHeight());
+            getSquare(c).getPaint().setColor(Color.YELLOW);
+            invalidate();
             return true;
         }
 
@@ -37,28 +39,70 @@ public class BoardView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-
+        this.canvas = canvas;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                Rect rect = getRect(new Coordinate(i, j), width, height);
-                boolean isEven = (i+j) % 2 == 1;
-                canvas.drawRect(rect, isEven ? whitePaint : blackPaint);
+                Coordinate c = new Coordinate(i, j);
+                Square s = getSquare(c);
+                s.updateRect();
+                canvas.drawRect(s.getRect(), s.getPaint());
             }
         }
     }
 
-    private Rect getRect(Coordinate c, int containerWidth, int containerHeight) {
+    private void initializeGrid() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Coordinate c = new Coordinate(i, j);
+                int color = board.getSquare(c).getColor() == chess.Color.White ? Color.WHITE : Color.GRAY;
+                grid[i][j] = new Square(c, color);
+            }
+        }
+    }
 
+    private Square getSquare(Coordinate c) {
+        return grid[c.getX()][c.getY()];
+    }
+
+    private Coordinate getCoordinate(float x, float y, int containerWidth, int containerHeight) {
         int rectWidth = containerWidth / 8;
         int rectHeight = containerHeight / 8;
-        int left = c.getX() * rectWidth;
-        int top = c.getY() * rectHeight;
-        int right = left + rectWidth;
-        int bottom = top + rectHeight;
+        int newX = (int) x / rectWidth;
+        int newY = 7 - ((int) y / rectHeight);
+        return new Coordinate(newX, newY);
+    }
 
-        return new Rect(left, top, right, bottom);
+    private class Square {
+
+        private Paint paint;
+        private Rect rect;
+        private Coordinate c;
+
+        public Square(Coordinate c, int color) {
+            rect = new Rect();
+            paint = new Paint();
+            paint.setColor(color);
+            this.c = c;
+        }
+
+        public void updateRect() {
+
+            int rectWidth = canvas.getWidth() / 8;
+            int rectHeight = canvas.getHeight() / 8;
+            int left = c.getX() * rectWidth;
+            int top = (7 - c.getY()) * rectHeight;
+            int right = left + rectWidth;
+            int bottom = top + rectHeight;
+
+            rect.set(left, top, right, bottom);
+        }
+
+        public Rect getRect() {
+            return rect;
+        }
+
+        public Paint getPaint() {
+            return paint;
+        }
     }
 }
--- app/src/main/java/com/qwezey/androidchess/MainActivity.java
+++ app/src/main/java/com/qwezey/androidchess/MainActivity.java
@@ -7,21 +7,31 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import chess.BadInputException;
+import game.Game;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-        );
-        setContentView(R.layout.activity_main);
+        try {
 
-        setContentView(new BoardView(this));
+            Game game = new Game();
 
+            super.onCreate(savedInstanceState);
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+            );
+            setContentView(R.layout.activity_main);
+
+            setContentView(new BoardView(this, game.getBoard()));
+
+        } catch (BadInputException e) {
+
+        }
     }
 }
