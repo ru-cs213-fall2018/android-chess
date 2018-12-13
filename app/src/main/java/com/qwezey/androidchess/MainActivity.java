dbbc14e5e1f9c6b33f8f223992098834719e0306
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-12-13 16:47:29 -0500

    no action bar for whole app

--- app/src/main/java/com/qwezey/androidchess/MainActivity.java
+++ app/src/main/java/com/qwezey/androidchess/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_IMMERSIVE
         );
-        getSupportActionBar().hide();
 
         // Add content
         setContentView(R.layout.activity_main);
