7e01b8c795e015c3f42fe38c514b31b2786f8da7
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-12-13 20:14:24 -0500

    added button to game

--- app/src/main/java/com/qwezey/androidchess/RecordingsActivity.java
+++ app/src/main/java/com/qwezey/androidchess/RecordingsActivity.java
@@ -1,11 +1,13 @@
 package com.qwezey.androidchess;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -24,6 +26,11 @@ public class RecordingsActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         RecordAdapter adapter = new RecordAdapter(this);
         recyclerView.setAdapter(adapter);
+    }
+
+    public void onGameButtonPress(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public class RecordAdapter extends RecyclerView.Adapter<RecordViewHolder> {
